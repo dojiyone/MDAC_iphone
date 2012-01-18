e2f9ac21bea8fdb544c0e88391add4444cf27a61
ken8979 <ken8979@gmail.com>
2012-01-18 17:38:48 +0900

    kast

--- DC_TEST01/PostDataEditViewController.h
+++ DC_TEST01/PostDataEditViewController.h
@@ -11,6 +11,23 @@
 
 @class TopViewController;
 
-@interface PostDataEditViewController : TopViewController
+@interface PostDataEditViewController : TopViewController <UIActionSheetDelegate>
+{
+    UINavigationController *nabi;
+    NSString *image_id;
+    NSString *re_value;
+    
+    IBOutlet UITextField *textField;
+    IBOutlet UITextView* textView;
+    
+    IBOutlet NSString* upCategory;
+    IBOutlet UIButton * ct_button;
+}
 
+@property (nonatomic,retain)NSString *re_value;
+@property (nonatomic,retain)NSString *image_id;
+@property (nonatomic,retain)UINavigationController *nabi;
+
+- (IBAction)post;
+- (IBAction)buttonDownCategorySelect:(id)sender;
 @end
--- DC_TEST01/PostDataEditViewController.m
+++ DC_TEST01/PostDataEditViewController.m
@@ -21,6 +21,10 @@
 
 @implementation PostDataEditViewController
 
+@synthesize re_value;
+@synthesize image_id;
+@synthesize nabi;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -32,23 +36,77 @@
 
 - (void)didReceiveMemoryWarning
 {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
 }
 
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
+    [scrollView setContentSize:CGSizeMake(320,2186)];
 
+}
+- (IBAction)post
+{   
+    int category=[upCategory intValue];
+    int tagCount=0;
+    
+    // タイトル未入力はエラー。
+    if([textField.text isEqualToString:@""]){
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@""
+                              message:@"タイトルが未入力です"
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+        [alert release];
+        return;
+    }
+    
+    if([textField.text length] >50){
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@""
+                              message:@"タイトル文字数は50文字までです"
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+        [alert release];
+        return;
+    }
+    
+    
+    //コメント未入力はエラー
+    if([textView.text isEqualToString:@""]){
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@""
+                              message:@"コメントが未入力です"
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+        [alert release];
+        return;
+    }
+    
+    if([textView.text length] > 1000){
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@""
+                              message:@"本文は1000文字までですです"
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+        [alert release];
+        return;
+    }
+    
+    loadingAPI* loadingapi=[[loadingAPI alloc] init];
+    //[loadingapi updatePostimage:post_img_id :category_id :textField.text :textView.text];
+}
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
+    //[super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -62,5 +120,37 @@
 - (void)viewDidAppear:(BOOL)animated
 {
 }
+
+- (IBAction)buttonDownCategorySelect:(id)sender//:(UIButton*)button
+{
+    UIActionSheet* actionSheet = [[UIActionSheet alloc] initWithTitle:nil
+                                                             delegate:self
+                                                    cancelButtonTitle:@"OK"
+                                               destructiveButtonTitle:nil
+                                                    otherButtonTitles:
+                                  @"dogs",
+                                  @"cats",
+                                  nil];
+    [actionSheet setActionSheetStyle:UIActionSheetStyleBlackTranslucent];
+    [actionSheet showInView:sender];
+    [actionSheet release];    
+}
+- (void) actionSheet:(UIActionSheet*)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    //番号別変更
+    //NSLog(@"%d",buttonIndex);
+    switch (buttonIndex) {
+        case 0:upCategory=@"0"; 
+            [ct_button setTitle:@"dogs" forState:UIControlStateNormal];
+            break;
+        case 1:upCategory=@"1"; 
+            [ct_button setTitle:@"cats" forState:UIControlStateNormal];
+            break;    
+        default:  break;
+    }
+    
+    
+}
+
 
 @end
