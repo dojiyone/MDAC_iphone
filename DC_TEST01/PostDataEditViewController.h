616b6cb7b47a75b14bcd4b172ee176366413b4bb
ken8979 <ken8979@gmail.com>
2012-01-18 14:52:11 +0900

    ijafo

---
+++ DC_TEST01/PostDataEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  PostDataEditViewController.h
+//  DC_TEST01
+//
+//  Created by ケンスケ スズキ on 12/01/18.
+//  Copyright (c) 2012年 CyberMuse, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostDataEditViewController : UIViewController
+
+@end
